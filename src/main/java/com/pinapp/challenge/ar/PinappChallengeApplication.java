c13e6edae166d4ac8dbb7f1c843a151e4ede6aa7
Martin Tapia <mtapia@palmatools.com>
2023-06-05 21:57:40 -0300

    initial project for pinapp

---
+++ src/main/java/com/pinapp/challenge/ar/PinappChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pinapp.challenge.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinappChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinappChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinapp/challenge/ar/PinappChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinapp.challenge.ar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinappChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
